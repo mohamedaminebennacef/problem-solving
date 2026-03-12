1c01c5f57982bc5e0baa53f504bce9ab3f51b3c4
mohamedaminebennacef <aminebennacef098@gmail.com>
2026-03-12 02:18:56 +0100

    c tcpc 25 math implementation 900

---
+++ tcpc25/c/c.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define F first
+#define S second
+#define em emplace_back
+#define mp make_pair
+#define sz(a) (int)(a.size())
+#define rall(x) (x).rbegin(), (x).rend()
+using ll = long long;
+#define endl "\n"
+// int a[500005];
+
+bool multi = true;
+
+long long roundup(long long a, long long b)
+{
+    return (a + b - 1) / b;
+}
+
+void solve(int tc)
+{
+    long long r, s, x;
+    cin >> r >> s >> x;
+    long long y = roundup(x * s * s, r * r);
+    while (y % 10 != 0)
+        y++;
+    cout << y << endl;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    if (multi)
+        cin >> t;
+    for (int i = 1; i <= t; i++)
+        solve(i);
+    return 0;
+}
