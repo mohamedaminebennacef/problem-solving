5bb8e868e16aacb47e06ee44b9528723ed931bbb
mohamedaminebennacef <aminebennacef098@gmail.com>
2026-06-16 19:34:07 +0100

    *1300 greedy strategy from a pattern for optimal packings by picking the largest available rectangle that can fit into the current layer

---
+++ 1300/greedy/b_box_fitting.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(a) a.begin(), a.end()
+using ll = long long;
+void solve(ll tc) {
+   ll n, w;
+   cin >> n >> w;
+   vector<ll> a(n);
+   map<ll, ll> cnt;
+   for (ll i = 0; i < n; i++) {
+       cin >> a[i];
+       cnt[a[i]]++;
+    }
+    ll h = 1, currlw = w, used = 0;
+    while (used != n) {
+        ll last = -1;
+        for (auto &[wi, f] : cnt) {
+            if (wi <= currlw && f != 0) 
+                last = wi;
+        }
+        if (last == -1) {
+            currlw = w;
+            h++;
+        }
+        else {
+            currlw -= last;
+            cnt[last]--;
+            used++;
+        }
+    }
+    cout << h << endl;    
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll t; cin >> t; for (ll tc = 1; tc <= t; tc++) solve(tc);
+    return 0;
+}
